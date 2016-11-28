47aeede8e2a01c926bf156636aaae07515b0ba16
Emmanuel <emmanuelanaya@outlook.com>
2016-11-27 22:30:20 -0600

    Complete args syntax validation

--- src/core.py
+++ src/core.py
@@ -1,6 +1,6 @@
-#! /usr/bin/python3
+#!/usr/bin/python3
 
-import sys, os, getpass
+import sys, os, getpass, re
 
 def msg_uso():
     print("\nShamir Secret Sharing Scheme (S^4)")
@@ -8,21 +8,28 @@ def msg_uso():
     
     print("\nPara cifrar, ejecute\n")
     print("\tssss -c <archivo_claro> n t <archivo_claves> <archivo_cifrado>\n")
-    print("\tdonde\t<archivo_claro> es el texto a cifrar")
-    print("\t\tn es el numero de claves a generar (n > 2)")
-    print("\t\tt es el numero de claves necesarias para decifrar (1 < t <= n)")
-    print("\t\t<archivo_claves> es la archivo donde se guardaran las claves")
-    print("\t\t<archivo_cifrado> es el archivo donde se guardara el texto cifrado\n\n")
+    print("\tdonde\t<archivo_claro>\t\tes el texto a cifrar")
+    print("\t\tn\t\t\tes el numero de claves a generar (n > 2)")
+    print("\t\tt\t\t\tes el numero de claves necesarias para decifrar (1 < t <= n)")
+    print("\t\t<archivo_claves>\tes el archivo donde se guardaran las claves")
+    print("\t\t<archivo_cifrado>\tes el archivo donde se guardara el texto cifrado\n\n")
 
     print("Para descifrar, ejecute\n")
     print("\tssss -d <archivo_cifrado> <archivo_claves>\n")
-    print("\tdonde\t<archivo_cifrado> es el archivo a descifrar")
-    print("\t\t<archivo_claves> es el archivo con las evaluaciones,"
-          " una por linea, de la forma \"x,f(x)\", sin comillas\n")
+    print("\tdonde\t<archivo_cifrado>\tes el archivo a descifrar")
+    print("\t\t<archivo_claves>\tes el archivo con las evaluaciones,"
+          " una por linea, de la\n\t\t\t\t\tforma \"x,f(x)\", sin comillas y"
+          " sin lineas en blanco\n")
 
     sys.exit()
 
-    
+
+def pairs_file_syntax(f):
+    lines = f.read().splitlines()
+    for l in lines:
+        if not re.match("-?\d+,-?\d+|", l): return False
+
+    return True
     
 def check_args_syntax(args):
 
@@ -46,10 +53,22 @@ def check_args_syntax(args):
         try:
             open(args[2], "rb").close()
             # revisar si <archivo_claves> cumple la sintaxis requerida
+            with open(args[3], "r") as f:
+                if not pairs_file_syntax(f): raise
         except:
             msg_uso()
+            raise
 
         return 2
 
+
+args = sys.argv
+
+if(check_args_syntax(args) == 1):
+    # cifrar la entrada
+    contraseña = getpass.getpass()
+else:
+    # descifrar la entrada
+    print("descifrar la entrada")
 
 
